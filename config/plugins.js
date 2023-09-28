d690fa69bd9be2f90526a83138d754e5d1c3524c
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2023-09-28 15:47:48 -0400

    adição do editorjs

---
+++ config/plugins.js
@@ -0,0 +1,32 @@
+module.exports = ({env}) => ({
+  // email: {
+  //   config: {
+  //     provider: 'nodemailer',
+  //     providerOptions: {
+  //       host: env('SMTP_HOST'),
+  //       port: env('SMTP_PORT'),
+  //       auth: {
+  //         user: env('SMTP_USERNAME'),
+  //         pass: env('SMTP_PASSWORD'),
+  //       },
+  //       // ... any custom nodemailer options
+  //     },
+  //     settings: {
+  //       defaultFrom: 'no-reply@strapi.io',
+  //       defaultReplyTo: 'no-reply@strapi.io',
+  //     },
+  //   },
+  // },
+  // email: {
+  //   config: {
+  //     provider: 'sendgrid',
+  //     providerOptions: {
+  //       apiKey: env('SENDGRID_API_KEY'),
+  //     },
+  //     settings: {
+  //       defaultFrom: 'myemail@protonmail.com',
+  //       defaultReplyTo: 'myemail@protonmail.com',
+  //     },
+  //   },
+  // },
+})
